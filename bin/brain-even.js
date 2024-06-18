cf4c763b45a60b584a63c6bad4dad8b0b7c2de5c
Darya <vareshka0101@mail.ru>
2024-06-18 15:35:00 +0300

    add new file

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import brainEvenGame from '../src/games/even.js';
+
+brainEvenGame();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import * as readlineSync from 'readline-sync';
-export const greetings = () => {
+import readlineSync from 'readline-sync';
+export default () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`${'Hello,'} ${userName}${'!'}`);
+  console.log(`Hello, ${userName}!`);
+  return userName;
 };
-
-
