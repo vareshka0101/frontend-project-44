72c7ef82db8306057f0f226af6ae828624fcccc5
vareshka0101 <142838334+vareshka0101@users.noreply.github.com>
2024-07-08 12:34:19 +0300

    Add calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,37 @@
+import randomNumber from './randomNum.js';
+import runGame from './index.js';
+
+const randomOperation = () => {
+    const operations = ['+', '-', '*'];
+    const index = randomNumber(operations.length);
+    return operations[index];
+  };
+  
+   const calculate = (num1, operation, num2) => {
+    switch (operation) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      default:
+        throw new Error(`Unknown operation: ${operation}`);
+    }
+  };
+
+  const generateQuestionAndAnswer = () => {
+    const num1 = randomNumber(50) + 1;
+    const num2 = randomNumber(50) + 1;
+    const operation = randomOperation();
+    const question = `${num1} ${operation} ${num2}`;
+    const correctAnswer = calculate(num1, operation, num2).toString();
+    return { question, correctAnswer };
+  };
+  
+  
+  const calcGame = () => {
+    runGame(generateQuestionAndAnswer);
+  };
+  
+  export default calcGame;
