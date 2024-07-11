d95415b03a2d31209902cc21f91e49c82fe7c92a
vareshka0101 <142838334+vareshka0101@users.noreply.github.com>
2024-07-11 11:05:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const welcome = () => {
   console.log("Welcome to the Brain Games!");
   const name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
-  return name;
 };
 
 const askQuestion = (question) => {
