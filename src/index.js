bdcd3150dd56685f139100e189622cd2320c0265
vareshka0101 <142838334+vareshka0101@users.noreply.github.com>
2024-07-15 14:21:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greetPlayer = () => {
   console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
+  const playerName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${playerName}!`);
   return playerName;
 };
