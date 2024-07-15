a5b13dc96f2aea26d5b07db6d0d8522f72684cd7
vareshka0101 <142838334+vareshka0101@users.noreply.github.com>
2024-07-15 14:09:55 +0300

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,7 +1,7 @@
 import runGame from '../index.js';
 import randomNumber from '../randomNum.js';
 
-const calculateGcd = (a, b) => (!b?a : calculateGcd(b, a % b));
+const calculateGcd = (a, b) => (!b ? a : calculateGcd(b, a % b));
 const getRound = () => {
   const num1 = randomNumber(30) + 1;
   const num2 = randomNumber(30) + 1;
